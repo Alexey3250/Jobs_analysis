6ca4516109254e7d1055954bc2d0df960e79e6e9
Alexey3250 <a.efimik@gmail.com>
2023-02-12 19:50:17 +0800

    1st comit

---
+++ spider.py
@@ -0,0 +1,36 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+job_title = 'analyst'
+region = 'Dubai'
+
+url = f'https://www.linkedin.com/jobs/search/?keywords={job_title}&location={region}'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+jobs = soup.find_all('li', class_='result-card')
+
+job_data = []
+for job in jobs:
+    title = job.find('h3', class_='result-card__title').text.strip()
+    company = job.find('a', class_='result-card__subtitle-link').text.strip()
+    location = job.find('span', class_='job-result-card__location').text.strip()
+    date_posted = job.find('time')['datetime']
+    job_url = job.find('a', class_='result-card__full-card-link')['href']
+    job_data.append({
+        'title': title,
+        'company': company,
+        'location': location,
+        'date_posted': date_posted,
+        'job_url': job_url
+    })
+
+# Export job data to CSV file
+with open('jobs.csv', 'w', newline='') as csvfile:
+    fieldnames = ['title', 'company', 'location', 'date_posted', 'job_url']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for job in job_data:
+        writer.writerow(job)
